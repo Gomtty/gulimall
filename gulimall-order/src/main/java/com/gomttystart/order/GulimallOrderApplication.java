e68f921f01c423404b8f415724974b46a59f439c
Gomtty <gomttystart@gmail.com>
2022-02-03 19:23:17 +0800

    新建子模块

---
+++ gulimall-order/src/main/java/com/gomttystart/order/GulimallOrderApplication.java
@@ -0,0 +1,13 @@
+package com.gomttystart.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallOrderApplication.class, args);
+    }
+
+}
